50963f3cb000d439088d59674ff503acabdd2e8c
Brant Fallin <brant.fallin@gmail.com>
2014-01-13 15:08:29 -0700

    Updated to se.orbit.core 0.1.3-rc3 which has case-insensitive task properties. Also replaced the custom SmartTypeConverter with the (much more flexible) VB.Conversion class.

--- SampleOrbitEventListenerService/Extensions/DictionaryExtensions.cs
+++ SampleOrbitEventListenerService/Extensions/DictionaryExtensions.cs
@@ -1,18 +1,34 @@
 using System;
 using System.Collections.Generic;
-using SampleOrbitEventListenerService.Converters;
+using Microsoft.VisualBasic;
+using NLog;
 
 namespace SampleOrbitEventListenerService.Extensions
 {
     static class DictionaryExtensions
     {
+        static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
         public static T Get<T>(this IDictionary<string, object> dictionary, string key)
         {
             T result = default(T);
             object value;
             if (dictionary != null && dictionary.TryGetValue(key, out value))
             {
-                result = (T)SmartTypeConverter.ConvertTo(typeof(T), value);
+                try
+                {
+                    result = (T)Conversion.CTypeDynamic(value, typeof(T));
+                }
+                catch (InvalidCastException e)
+                {
+                    string errorMessage = string.Format(
+                        "Cannot convert key '{0}' value '{1}' from {2} to {3}.",
+                        key,
+                        value,
+                        (value == null) ? "null" : value.GetType().Name,
+                        typeof(T).Name);
+                    Log.Warn(errorMessage, e);
+                }
             }
             return result;
         }
